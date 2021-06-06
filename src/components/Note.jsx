a064ed476a6c390e31a2b26477545d8e2787a174
Rithik Restan <84955575+RITHIKRESTAN@users.noreply.github.com>
2021-06-06 16:31:55 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function Note() {
+  
+  return (
+  <div className="note">
+    <h1>
+      Javascript And React.JS
+    </h1>
+    <p>
+      This was an Amazing Bootcamp By ShapeAI And The Teacher Was The most Amazing One. Shaurya Sinha the teacher who teach us about React.Js and about HTML from scratch 
+to the very end .He was very good in teaching,It was very best bootcamp and I get to know different things about coding and about React JS . 
+IT was a very good experience.
+    </p>
+  </div>
+  );
+}
+export default Note;
---
+++ src/components/Note1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Note1() {
+  
+  return (
+  <div className="note">
+    <h1>
+      Python And Machine Learning
+    </h1>
+    <p>
+      It was an Amazing bootcamp Conducted by the ShapeAI. 
+I get to laern from the scratch to a normal level. I AM Also looking forward to for the next awesome bootcamps.
+    </p>
+  </div>
+  );
+}
+export default Note1;
